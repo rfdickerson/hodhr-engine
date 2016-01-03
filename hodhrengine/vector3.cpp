23316584f750b7c2af4056700ba492a1a75cffff
Robert F. Dickerson <rfdickerson@gmail.com>
2016-01-02 18:22:53 -0600

    added a vector abstraction

---
+++ hodhrengine/vector3.cpp
@@ -0,0 +1,43 @@
+
+#include "vector3.h"
+
+#include <glm/common.hpp>
+#include <glm/geometric.hpp>
+
+Vector3 Vector3::back() {
+  return Vector3(0,0,-1);
+}
+
+Vector3 Vector3::down() {
+  return Vector3(0,-1,0);
+}
+
+Vector3 Vector3::forward() {
+  return Vector3(0,0,1);
+}
+
+Vector3 Vector3::up() {
+  return Vector3(0,1,0);
+}
+
+Vector3 Vector3::zero() {
+  return Vector3(0,0,0);
+}
+
+Vector3::Vector3(float x, float y, float z) {
+    _vector = glm::vec3(x, y, z);
+}
+
+float Vector3::magnitude() {
+  return glm::length(this->_vector);
+}
+
+float Vector3::dot(const Vector3 &a, const Vector3 &b) {
+
+  return glm::dot(a._vector, b._vector);
+
+}
+
+std::string Vector3::toString() {
+  
+}
---
+++ hodhrengine/vector3.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <glm/common.hpp>
+
+#include "common.h"
+
+/**
+ * Vector3
+ * Representation of 3D vectors and points.
+ * This structure is used throughout to pass 3D positions and directions around.
+ **/
+class Vector3 {
+
+public:
+
+  Vector3(float x, float y, float z);
+  ~Vector3();
+
+  // Shorthand for writing Vector3(0,0,-1).
+  static Vector3 back();
+  // Shorthand for writing Vector3(0,-1,0).
+  static Vector3 down();
+  static Vector3 forward();
+  
+  static Vector3 left();
+  
+  static Vector3 one();
+  
+  static Vector3 right();
+
+  static Vector3 up();
+  
+  static Vector3 zero();
+  
+  // returns the length of this vector.
+  float magnitude();
+
+  // returns this vector with a magnitude of 1.
+  Vector3 normalized();
+
+  // returns the squared length of this vector.
+  float sqrMagnitude();
+
+  float x();
+  float y();
+  float z();
+
+  std::string toString();
+
+  static float dot(const Vector3 & a, const Vector3 & b);
+  static float distance(const Vector3 &a, const Vector3 &b);
+  
+
+protected:
+
+
+  glm::vec3 _vector;
+
+};
